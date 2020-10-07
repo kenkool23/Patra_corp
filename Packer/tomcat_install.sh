14c146d8f6d78f02b5a7352087fe6e16a7e4badf
kenkool23 <62091494+kenkool23@users.noreply.github.com>
2020-10-06 20:44:11 -0400

    Update tomcat_install.sh

--- Packer/tomcat_install.sh
+++ Packer/tomcat_install.sh
@@ -6,3 +6,5 @@ sudo cp -r /usr/share/tomcat9-admin/* /var/lib/tomcat9/webapps/ -v
 sudo chmod 777 /var/lib/tomcat9/conf/tomcat-users.xml
 sudo echo "<role rolename="manager-script"/> <user username="tomcat" password="password" roles="manager-script"/>" > /var/lib/tomcat9/conf/tomcat-users.xml
 sudo systemctl restart tomcat9
+
+#This is a simple shell script to install and configure Tomcat Server. This will be invoked by Packer to build a custom image
